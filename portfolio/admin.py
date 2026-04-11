7ad2a0519a6a225efe1fbbfbe88169d85ce57ebc
afonsorodrigues <a22408702@alunos.ulht.pt>
2026-04-11 19:59:04 +0000

    Implementado modelo Docente e relação Many-to-Many com UnidadeCurricular

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Licenciatura, UnidadeCurricular
+from .models import Licenciatura, UnidadeCurricular, Docente
 
 admin.site.register(Licenciatura)
+admin.site.register(Docente)
 admin.site.register(UnidadeCurricular)
+
--- portfolio/models.py
+++ portfolio/models.py
@@ -9,6 +9,13 @@ class Licenciatura(models.Model):
     def __str__(self):
         return self.nome
 
+class Docente(models.Model):
+    nome = models.CharField(max_length=100)
+    biografia = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.nome
+
 class UnidadeCurricular(models.Model):
     nome = models.CharField(max_length=100)
     ano = models.IntegerField()
@@ -17,6 +24,7 @@ class UnidadeCurricular(models.Model):
     imagem = models.ImageField(upload_to='cadeiras/', null=True, blank=True)
     link = models.URLField(max_length=300, null=True, blank=True)
     licenciatura = models.ForeignKey(Licenciatura, on_delete=models.CASCADE, related_name='unidades_curriculares')
+    docentes = models.ManyToManyField(Docente, related_name='unidades_curriculares')
 
     def __str__(self):
         return self.nome
